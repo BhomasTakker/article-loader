d5bc285ac4ff53982ffce812e5ca5efd334a897e
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-03-15 20:15:04 +0000

    create map

---
+++ sources/news/map.ts
@@ -0,0 +1,26 @@
+import { UK_1, UK_2 } from "./articles/uk";
+import { US_1, US_2, US_3, US_4 } from "./articles/us";
+import {
+	WORLD_1,
+	WORLD_2,
+	WORLD_3,
+	WORLD_4,
+	WORLD_5,
+	WORLD_6,
+} from "./articles/world";
+
+export const sourcesMap = new Map<string, object>([
+	["uk-1", UK_1],
+	["uk-2", UK_2],
+	["us-1", US_1],
+	["us-2", US_2],
+	["us-3", US_3],
+	["us-4", US_4],
+	["world-1", WORLD_1],
+	["world-2", WORLD_2],
+	["world-3", WORLD_3],
+	["world-4", WORLD_4],
+	["world-5", WORLD_5],
+	["world-6", WORLD_6],
+	// ['youtube', YOUTUBE],
+]);
