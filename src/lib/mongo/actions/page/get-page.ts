cd44db2cec17f75f6c86c829a9fbde9aa0ecabfc
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-04-22 13:08:39 +0100

    ping pages via admin user

--- src/lib/mongo/actions/page/get-page.ts
+++ src/lib/mongo/actions/page/get-page.ts
@@ -1,6 +1,18 @@
-import { HydratedDocument } from "mongoose";
+import mongoose, { HydratedDocument } from "mongoose";
 import Page from "../../../../models/Page";
 import { IPage } from "../../../../types/page/page";
+
+export const getPagesByUser = async (userId: string) => {
+	try {
+		const userIdAsObjectId = new mongoose.Types.ObjectId(userId);
+		const pages = await Page.find({ creator: userIdAsObjectId }).lean();
+		if (!pages) return null;
+		return pages as HydratedDocument<IPage>[];
+	} catch (err) {
+		console.error("Error fetching pages by user:", err);
+		return null;
+	}
+};
 
 export const getPageByRoute = async (route: string) => {
 	try {
