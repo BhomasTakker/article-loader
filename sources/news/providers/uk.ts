2ecde6b61028935ada4d01f849c44ffdb7691107
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-05-03 17:04:12 +0100

    update uk providers

--- sources/news/providers/uk.ts
+++ sources/news/providers/uk.ts
@@ -3,6 +3,7 @@ export const bbcNews = {
 	name: "BBC",
 	description: "BBC News is a British free-to-air television news channel.",
 	url: "https://www.bbc.co.uk",
+	logo: "https://static.files.bbci.co.uk/ws/simorgh-assets/public/news/images/metadata/poster-1024x576.png",
 	rating: 82,
 	leaning: 0,
 };
@@ -13,22 +14,16 @@ export const guardian = {
 	description:
 		"The Guardian is a British news and media website owned by the Guardian Media Group.",
 	url: "https://www.theguardian.com",
+	logo: "https://assets.guim.co.uk/static/frontend/icons/homescreen/apple-touch-icon-512.png",
 	rating: 85,
 	leaning: -0.1,
-};
-export const times = {
-	origin: "UK",
-	name: "The Times",
-	description: "The Times is a British daily national newspaper.",
-	url: "https://www.thetimes.co.uk/",
-	rating: 75,
-	leaning: 0.1,
 };
 export const telegraph = {
 	origin: "UK",
 	name: "The Telegraph",
 	description: "The Telegraph is a British newspaper.",
 	url: "https://www.telegraph.co.uk/",
+	logo: "https://www.telegraph.co.uk/etc.clientlibs/settings/wcm/designs/telegraph/core/clientlibs/core/resources/icons/tmg-share-img.svg",
 	rating: 70,
 	leaning: 0.2,
 };
@@ -38,6 +33,7 @@ export const mirror = {
 	name: "The Mirror",
 	description: "The Mirror is a British national daily tabloid newspaper.",
 	url: "https://www.mirror.co.uk/",
+	logo: "https://s2-prod.mirror.co.uk/@trinitymirrordigital/chameleon-branding/publications/mirror/img/logo-mirror-social-sharing.png",
 	rating: 82,
 	leaning: -0.3,
 };
@@ -47,6 +43,7 @@ export const dailyMail = {
 	name: "The Daily Mail",
 	description: "The Daily Mail is a British daily middle-market newspaper.",
 	url: "https://www.dailymail.co.uk/",
+	logo: "https://www.dailymail.co.uk/static/icons/180.png",
 	rating: 65,
 	leaning: 0.5,
 };
@@ -56,6 +53,7 @@ export const metro = {
 	name: "Metro",
 	description: "Metro is a British free daily newspaper.",
 	url: "https://metro.co.uk/",
+	logo: "https://metro.co.uk/wp-content/client-mu-plugins/metro-branding/images/m-black-share.png",
 	rating: 80,
 	leaning: 0.2,
 };
@@ -65,6 +63,7 @@ export const eveningStandard = {
 	name: "The Evening Standard",
 	description: "The Evening Standard is a local, free daily newspaper.",
 	url: "https://www.standard.co.uk/",
+	logo: "https://www.standard.co.uk/img/shortcut-icons/icon-512x512.png",
 	rating: 80,
 	leaning: 0.3,
 };
@@ -74,6 +73,7 @@ export const theSun = {
 	name: "The Sun",
 	description: "The Sun is a British tabloid newspaper.",
 	url: "https://www.thesun.co.uk/",
+	logo: "https://www.thesun.co.uk/wp-content/uploads/2016/04/icon-e1459786005667.png?strip=all",
 	rating: 65,
 	leaning: 0.5,
 };
@@ -83,6 +83,7 @@ export const skyNews = {
 	name: "Sky News",
 	description: "Sky News is a British free-to-air television news channel.",
 	url: "https://news.sky.com/",
+	logo: "https://news.sky.com/resources/apple-touch-icon.png?v=2",
 	rating: 85,
 	leaning: 0,
 };
@@ -92,6 +93,7 @@ export const independent = {
 	name: "The Independent",
 	description: "The Independent is a British online newspaper.",
 	url: "https://www.independent.co.uk/",
+	logo: "https://www.independent.co.uk/img/shortcut-icons/icon-96x96.png",
 	rating: 85,
 	leaning: -0.1,
 };
@@ -101,6 +103,7 @@ export const huffpost = {
 	name: "The Huffington Post",
 	description: "HuffPost UK is a British news and opinion website.",
 	url: "https://www.huffingtonpost.co.uk/",
+	logo: "https://img.huffingtonpost.com/asset/5f3448f32200001eb13888de.jpg",
 	rating: 75,
 	leaning: -0.3,
 };
@@ -110,6 +113,7 @@ export const dailyRecord = {
 	name: "The Daily Record",
 	description: "The Daily Record is a Scottish tabloid newspaper.",
 	url: "https://www.dailyrecord.co.uk/",
+	logo: "https://s2-prod.dailyrecord.co.uk/@trinitymirrordigital/chameleon-branding/publications/dailyrecord/img/logo-dailyrecord-social-sharing.png",
 	rating: 80,
 	leaning: 0,
 };
@@ -119,6 +123,7 @@ export const politicsCoUk = {
 	name: "politics.co.uk",
 	description: "Politics.co.uk is a British political news website.",
 	url: "https://www.politics.co.uk/",
+	logo: "https://www.politics.co.uk/wp-content/themes/politicsuk/library/images/apple-touch-icon.png",
 	rating: 80,
 	leaning: -0.2,
 };
@@ -128,6 +133,7 @@ export const theConversation = {
 	name: "The Conversation",
 	description: "The Conversation is a British independent news website.",
 	url: "https://theconversation.com",
+	logo: "https://images.theconversation.com/files/581997/original/9275d5c0-7dd5-4ddf-b50b-98d537e016f4.png",
 	rating: 90,
 	leaning: 0,
 };
@@ -137,6 +143,7 @@ export const manchesterEveningNews = {
 	name: "The Manchester Evening News",
 	description: "The Manchester Evening News is a regional daily newspaper.",
 	url: "https://www.manchestereveningnews.co.uk/",
+	logo: "https://s2-prod.manchestereveningnews.co.uk/@trinitymirrordigital/chameleon-branding/publications/men/img/logo-men-social-sharing.png",
 	rating: 85,
 	leaning: -0.4,
 };
@@ -146,6 +153,7 @@ export const birminghamLive = {
 	name: "Birmingham Live",
 	description: "Birmingham Live is a regional news website.",
 	url: "https://www.birminghammail.co.uk/",
+	logo: "https://s2-prod.birminghammail.co.uk/@trinitymirrordigital/chameleon-branding/publications/birminghammail/img/logo-birminghamlive-social-sharing.png",
 	rating: 80,
 	leaning: 0.3,
 };
@@ -155,6 +163,7 @@ export const thePinkNews = {
 	name: "The Pink News",
 	description: "The Pink News is a British news website.",
 	url: "https://www.thepinknews.com/",
+	logo: "https://www.thepinknews.com/wp-content/themes/pinknews/.src/images/favicons/mstile-310x310.png",
 	rating: 75,
 	leaning: -0.3,
 };
@@ -164,6 +173,7 @@ export const northernIrelandWorld = {
 	name: "Northern Ireland World",
 	description: "Northern Ireland World is a regional news website.",
 	url: "https://northernirelandworld.com/",
+	logo: "https://www.northernirelandworld.com/img/placeholder.png",
 	rating: 80,
 	leaning: 0,
 };
@@ -173,6 +183,7 @@ export const walesOnline = {
 	name: "Wales Online",
 	description: "Wales Online is a regional news website.",
 	url: "https://www.walesonline.co.uk/",
+	logo: "https://s2-prod.walesonline.co.uk/@trinitymirrordigital/chameleon-branding/publications/walesonline/img/logo-walesonline-social-sharing.png",
 	rating: 85,
 	leaning: -0.2,
 };
@@ -182,6 +193,7 @@ export const heraldScotland = {
 	name: "Herald Scotland",
 	description: "Herald Scotland UK is a regional news website.",
 	url: "https://www.heraldscotland.com/",
+	logo: "https://www.heraldscotland.com/resources/images/small_site_logo/",
 	rating: 85,
 	leaning: -0.3,
 };
@@ -191,6 +203,7 @@ export const gbNews = {
 	name: "GB News",
 	description: "GB News is a British news channel and website.",
 	url: "https://www.gbnews.uk/",
+	logo: "https://www.gbnews.com/media-library/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8zMzEwMDg4MS9vcmlnaW4ucG5nIiwiZXhwaXJlc19hdCI6MTc5NzY2ODQ5OX0.yPkj1VxfBk72KU9hNSQwXmOcRlz79vywCKtwhL4rbwA/image.png?width=210",
 	rating: 45,
 	leaning: 0.7,
 };
@@ -200,6 +213,7 @@ export const theStar = {
 	name: "The Star",
 	description: "The Star is a British regional daily newspaper for Sheffield.",
 	url: "https://www.thestar.co.uk/",
+	logo: "https://www.thestar.co.uk/img/placeholder.png",
 	rating: 80,
 	leaning: 0,
 };
@@ -209,6 +223,7 @@ export const sheffieldTribune = {
 	name: "The Sheffield Tribune",
 	description: "The Sheffield Tribune is a British regional daily newspaper.",
 	url: "https://www.sheffieldtribune.co.uk/",
+	logo: "https://www.sheffieldtribune.co.uk/content/images/2024/12/Sheffeildcover02logo2-2.jpg",
 	rating: 80,
 	leaning: 0,
 };
@@ -218,6 +233,7 @@ export const tldr = {
 	name: "TLDR",
 	description: "TLDR is a British news website.",
 	url: "https://tldrnews.co.uk/",
+	logo: "https://tldrnews.co.uk/wp-content/uploads/2023/09/cropped-UK-192x192.png",
 	rating: 75,
 	leaning: 0,
 };
@@ -226,6 +242,7 @@ export const theTimes = {
 	name: "The Times",
 	description: "The Times is a British daily national newspaper.",
 	url: "https://www.thetimes.com/",
+	logo: "https://www.thetimes.com/subscribe/wp-content/uploads/sites/9/2018/04/cropped-T_Favicon_192x1921.png?w=192",
 	rating: 75,
 	leaning: 0.1,
 };
@@ -235,6 +252,7 @@ export const theLiverpoolPost = {
 	name: "The Liverpool Post",
 	description: "The Liverpool Post is a British regional daily newspaper.",
 	url: "https://www.livpost.co.uk/",
+	logo: "https://www.livpost.co.uk/content/images/size/w256h256/2025/02/P-only.png",
 	rating: 80,
 	leaning: 0,
 };
@@ -244,6 +262,7 @@ export const theManchesterMill = {
 	name: "The Manchester Mill",
 	description: "The Manchester Mill is a British regional daily newspaper.",
 	url: "https://manchestermill.co.uk/",
+	logo: "https://manchestermill.co.uk/content/images/size/w256h256/2024/12/Untitled-design--25-.png",
 	rating: 80,
 	leaning: 0,
 };
@@ -253,6 +272,7 @@ export const ft = {
 	name: "Financial Times",
 	description: "The Financial Times is a British daily newspaper.",
 	url: "https://www.ft.com/",
+	logo: "https://www.ft.com/__origami/service/image/v2/images/raw/ftlogo-v1%3Abrand-ft-logo-square-coloured?source=update-logos&format=png&width=180&height=180",
 	rating: 75,
 	leaning: 0.1,
 };
@@ -262,6 +282,7 @@ export const novara = {
 	name: "Novara Media",
 	description: "Novara Media is a British independent media company.",
 	url: "https://novaramedia.com/",
+	logo: "https://novaramedia.com/wp-content/uploads/2024/02/OG_Main-Website-1200x630.png",
 	rating: 85,
 	leaning: -0.4,
 };
