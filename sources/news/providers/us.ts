9a6f222f8dc7109a2b18e8e177f3c46d3363fce8
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-05-03 18:53:49 +0100

    update us providers

--- sources/news/providers/us.ts
+++ sources/news/providers/us.ts
@@ -1,10 +1,9 @@
-import { NewsSources } from "../sources";
-
 export const cnn = {
 	origin: "USA",
 	name: "CNN",
 	description: "CNN is an American news-based pay television channel.",
 	url: "https://edition.cnn.com/",
+	logo: "https://media.cnn.com/api/v1/images/cnn/cnn-placeholder-image.jpeg",
 	rating: 85,
 	leaning: -0.1,
 };
@@ -15,6 +14,7 @@ export const fox = {
 	description:
 		"Fox News is an American multinational conservative cable news television channel.",
 	url: "https://www.foxnews.com/",
+	logo: "https://static.foxnews.com/static/orion/styles/img/fox-news/og/og-fox-news.png",
 	rating: 65,
 	leaning: 0.5,
 };
@@ -25,6 +25,7 @@ export const wallStreetJournal = {
 	description:
 		"The Wall Street Journal is an American business-focused, English-language international daily newspaper.",
 	url: "https://www.wsj.com/",
+	logo: "https://s.wsj.net/media/wsj_apple-touch-icon-180x180.png",
 	rating: 85,
 	leaning: 0,
 };
@@ -34,6 +35,7 @@ export const yahoo = {
 	name: "Yahoo News",
 	description: "Yahoo News is an American news website.",
 	url: "https://news.yahoo.com/",
+	logo: "https://s.yimg.com/cv/apiv2/social/images/yahoo_default_logo.png",
 	rating: 75,
 	leaning: 0,
 };
@@ -44,6 +46,7 @@ export const npr = {
 	description:
 		"NPR is an American privately and publicly funded non-profit membership media organization.",
 	url: "https://www.npr.org/",
+	logo: "https://media.npr.org/chrome/favicon/favicon-180x180.png",
 	rating: 90,
 	leaning: -0.2,
 };
@@ -53,6 +56,7 @@ export const washingtonTimes = {
 	name: "Washington Times",
 	description: "The Washington Times is an American daily newspaper.",
 	url: "https://www.washingtontimes.com/",
+	logo: "https://www.washingtontimes.com/static/icons/apple-touch-icon.38e481a2f5d5.png",
 	rating: 75,
 	leaning: 0.4,
 };
@@ -62,6 +66,7 @@ export const washingtonPost = {
 	name: "The Washington Post",
 	description: "The Washington Post is an American daily newspaper.",
 	url: "https://www.washingtonpost.com/",
+	logo: "https://www.washingtonpost.com/touch-icon-iphone.png",
 	rating: 85,
 	leaning: -0.1,
 };
@@ -71,6 +76,7 @@ export const time = {
 	name: "Time",
 	description: "Time is an American weekly news magazine.",
 	url: "https://time.com/",
+	logo: "https://api.time.com/wp-content/themes/time2014/img/time-logo-og.png",
 	rating: 85,
 	leaning: -0.1,
 };
@@ -80,6 +86,7 @@ export const laTimes = {
 	name: "LA Times",
 	description: "The Los Angeles Times is an American daily newspaper.",
 	url: "https://www.latimes.com/",
+	logo: "https://ca-times.brightspotcdn.com/dims4/default/64e287b/2147483647/strip/true/crop/2400x1260+0+0/resize/1200x630!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Fdf%2F45%2F57d858144a2a88575fa2b03080bb%2Flatlogo-ss.jpg",
 	rating: 85,
 	leaning: -0.2,
 };
@@ -89,6 +96,7 @@ export const mercuryNews = {
 	name: "Mercury News",
 	description: "The Mercury News is an American daily newspaper.",
 	url: "https://www.mercurynews.com/",
+	logo: "https://www.mercurynews.com/wp-content/uploads/2021/08/mn-logo-sm.png",
 	rating: 80,
 	leaning: 0.2,
 };
@@ -98,6 +106,7 @@ export const usatoday = {
 	name: "USA Today",
 	description: "USA Today is an American daily newspaper.",
 	url: "https://www.usatoday.com/",
+	logo: "https://www.gannett-cdn.com/gannett-web/properties/usatoday/logos-and-branding/logo-icon.png",
 	rating: 85,
 	leaning: -0.1,
 };
@@ -106,6 +115,7 @@ export const usNews = {
 	name: "US News",
 	description: "US News is an American news website.",
 	url: "https://www.usnews.com/",
+	logo: "https://www.usnews.com/static-atlas/assets/img/social-logo-world-report.png",
 	rating: 85,
 	leaning: -0.1,
 };
@@ -116,6 +126,7 @@ export const cbs = {
 	description:
 		"CBS News is an American television and radio network news division.",
 	url: "https://www.cbsnews.com/",
+	logo: "https://assets1.cbsnewsstatic.com/hub/i/2022/01/22/4fc70efe-a2d7-4eb3-8f23-05db514ef3bb/news-slate-stream.jpg",
 	rating: 85,
 	leaning: -0.1,
 };
@@ -125,6 +136,7 @@ export const abc = {
 	name: "ABC",
 	description: "ABC News is an American news division.",
 	url: "https://abcnews.go.com/",
+	logo: "https://s.abcnews.com/images/US/abc_news_default_2000x2000_update_16x9_992.jpg",
 	rating: 85,
 	leaning: -0.1,
 };
@@ -134,6 +146,7 @@ export const cnbc = {
 	name: "CNBC",
 	description: "CNBC is an American pay television business news channel.",
 	url: "https://www.cnbc.com/",
+	logo: "https://sc.cnbcfm.com/applications/cnbc.com/staticcontent/img/cnbc_logo_new.png?v=1524171804&w=1920&h=1080",
 	rating: 85,
 	leaning: -0.1,
 };
@@ -143,6 +156,7 @@ export const breitbart = {
 	name: "Breitbart",
 	description: "Breitbart News is an American far-right syndicated news.",
 	url: "https://www.breitbart.com/",
+	logo: "https://media.breitbart.com/media/2018/06/BB-logo-highres.jpg",
 	rating: 45,
 	leaning: 0.7,
 };
@@ -152,6 +166,7 @@ export const bostonHerald = {
 	name: "Boston Herald",
 	description: "The Boston Herald is an American daily newspaper.",
 	url: "https://www.bostonherald.com/",
+	logo: "https://www.bostonherald.com/wp-content/uploads/2018/11/Herald_BridgePhoto.jpg",
 	rating: 80,
 	leaning: 0.2,
 };
@@ -161,6 +176,7 @@ export const denverPost = {
 	name: "Denver Post",
 	description: "The Denver Post is an American daily newspaper.",
 	url: "https://www.denverpost.com/",
+	logo: "https://www.denverpost.com/wp-content/uploads/2020/11/denverpost.jpg",
 	rating: 80,
 	leaning: 0.2,
 };
@@ -170,6 +186,7 @@ export const newYorkPost = {
 	name: "New York Post",
 	description: "The New York Post is an American daily newspaper.",
 	url: "https://nypost.com/",
+	logo: "https://nypost.com/wp-content/themes/gtf/static/images/default/nypost.png",
 	rating: 80,
 	leaning: 0.2,
 };
@@ -179,6 +196,7 @@ export const theHill = {
 	name: "The Hill",
 	description: "The Hill is an American news website.",
 	url: "https://thehill.com/",
+	logo: "https://thehill.com/wp-content/uploads/sites/2/2023/03/cropped-favicon-512px-1.png?strip=1&w=250",
 	rating: 85,
 	leaning: -0.1,
 };
@@ -188,6 +206,7 @@ export const politico = {
 	name: "Politico",
 	description: "Politico is an American political journalism company.",
 	url: "https://www.politico.com/",
+	logo: "https://static.politico.com/3d/ee/f4d549cf4e548af6712990ce5b9e/politico-cms-shared-graphic-1120x629.jpg",
 	rating: 85,
 	leaning: -0.1,
 };
@@ -197,6 +216,7 @@ export const vox = {
 	name: "Vox",
 	description: "Vox is an American news and opinion website.",
 	url: "https://www.vox.com/",
+	logo: "https://www.vox.com/static-assets/default-og-image.png",
 	rating: 85,
 	leaning: -0.1,
 };
@@ -206,6 +226,7 @@ export const buzzfeed = {
 	name: "Buzzfeed",
 	description: "Buzzfeed News is an American news website.",
 	url: "https://www.buzzfeednews.com/",
+	logo: "https://www.buzzfeed.com/static-assets/img/buzzfeed_arrow.e86a786d9e5e2250e1ed3e0ec95ba42d.png",
 	rating: 75,
 	leaning: -0.3,
 };
@@ -215,6 +236,7 @@ export const theAtlantic = {
 	name: "The Atlantic",
 	description: "The Atlantic is an American magazine.",
 	url: "https://www.theatlantic.com/",
+	logo: "https://cdn.theatlantic.com/static/theatlantic/img/lacroix-default-thumbnail.png",
 	rating: 85,
 	leaning: -0.1,
 };
@@ -224,6 +246,7 @@ export const seatleTimes = {
 	name: "Seattle Times",
 	description: "The Seattle Times is an American daily newspaper.",
 	url: "https://www.seattletimes.com/",
+	logo: "https://www.seattletimes.com/apple-touch-icon.png?v=7kovnr5xE4",
 	rating: 85,
 	leaning: -0.1,
 };
@@ -233,6 +256,7 @@ export const theIntercept = {
 	name: "The Intercept",
 	description: "The Intercept is an American news website.",
 	url: "https://theintercept.com/",
+	logo: "https://theintercept.com/wp-content/themes/intercept/assets/icons/apple-touch-icon-180x180.png",
 	rating: 85,
 	leaning: -0.1,
 };
@@ -242,6 +266,7 @@ export const verge = {
 	name: "The Verge",
 	description: "The Verge is an American technology news website.",
 	url: "https://www.theverge.com/",
+	logo: "https://platform.theverge.com/wp-content/uploads/sites/2/2024/10/the_verge_social_share.png",
 	rating: 85,
 	leaning: -0.1,
 };
@@ -251,6 +276,7 @@ export const theDailyBeast = {
 	name: "The Daily Beast",
 	description: "The Daily Beast is an American news website.",
 	url: "https://www.thedailybeast.com/",
+	logo: "https://www.thedailybeast.com/resizer/v2/https%3A%2F%2Fstatic.themebuilder.aws.arc.pub%2Fthedailybeast%2F1741381130350.png?smart=true&auth=9944ab8bbceee1a01501dd4684bb6b782f7e8557cdb70bf4b20d962b29d17c75&width=1200",
 	rating: 85,
 	leaning: -0.1,
 };
@@ -260,6 +286,7 @@ export const theNewYorker = {
 	name: "The New Yorker",
 	description: "The New Yorker is an American magazine.",
 	url: "https://www.newyorker.com/",
+	logo: "https://media.newyorker.com/photos/68150b6f63a00546652996f0/16:9/w_1280,c_limit/Tolentino_Brain.jpg",
 	rating: 85,
 	leaning: -0.1,
 };
@@ -269,6 +296,7 @@ export const theOnion = {
 	name: "The Onion",
 	description: "The Onion is an American satirical digital media company.",
 	url: "https://www.theonion.com/",
+	logo: "https://theonion.com/wp-content/uploads/2024/08/NRH-Share-Card.png",
 	rating: 75,
 	leaning: 0,
 };
@@ -278,6 +306,7 @@ export const theDailyCaller = {
 	name: "The Daily Caller",
 	description: "The Daily Caller is an American news and opinion website.",
 	url: "https://dailycaller.com/",
+	logo: "https://dailycaller.com/files/DailyCaller-Icon.png",
 	rating: 45,
 	leaning: 0.7,
 };
@@ -287,7 +316,8 @@ export const theFederalist = {
 	name: "The Federalist",
 	description: "The Federalist is an American conservative online magazine.",
 	url: "https://thefederalist.com/",
-	rating: 45,
+	logo: "https://thefederalist.com/wp-content/uploads/2021/12/fdrlst-default-thumbnail.jpg",
+	rating: 40,
 	leaning: 0.7,
 };
 
@@ -297,6 +327,7 @@ export const theAmericanConservative = {
 	description:
 		"The American Conservative is an American conservative magazine.",
 	url: "https://www.theamericanconservative.com/",
+	logo: "https://www.theamericanconservative.com/wp-content/uploads/2022/07/social-sharing.png",
 	rating: 45,
 	leaning: 0.7,
 };
@@ -306,6 +337,7 @@ export const theBulwark = {
 	name: "The Bulwark",
 	description: "The Bulwark is an American news website.",
 	url: "https://thebulwark.com/",
+	logo: "https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F9304d804-0372-498e-b7b1-b09155c9840a%2Fapple-touch-icon-180x180.png",
 	rating: 45,
 	leaning: 0.7,
 };
@@ -315,6 +347,7 @@ export const newYorkTimes = {
 	name: "New York Times",
 	description: "The New York Times is an American newspaper.",
 	url: "https://www.nytimes.com/",
+	logo: "https://static01.nyt.com/newsgraphics/images/icons/defaultPromoCrop.png",
 	rating: 85,
 	leaning: -0.1,
 };
@@ -324,6 +357,7 @@ export const theEconomist = {
 	name: "The Economist",
 	description: "The Economist is an American newspaper.",
 	url: "https://www.economist.com/",
+	logo: "https://economist.prod.limio.com/public/8e54554b-dc88-48d8-845d-4cad0e847c00/favicon.f1ea9088.png",
 	rating: 85,
 	leaning: 0,
 };
