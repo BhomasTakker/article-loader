6c039af763c2aa356d4fc92fd360880199b37d93
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-05-11 13:56:44 +0100

    update cron jobs for rss

--- src/cron/rss/rss-cron.ts
+++ src/cron/rss/rss-cron.ts
@@ -1,10 +1,41 @@
 import { DIDDY_TRIAL } from "../../../sources/audio/podbean/news/features";
-import { UK_3 } from "../../../sources/news/articles/uk";
-import { US_5 } from "../../../sources/news/articles/us";
-import { WORLD_7 } from "../../../sources/news/articles/world";
-import { UK_VIDEO_2 } from "../../../sources/news/videos/uk";
-import { US_VIDEO_2 } from "../../../sources/news/videos/us";
-import { WORLD_VIDEO_2 } from "../../../sources/news/videos/world";
+import {
+	BITES_UK,
+	NEWS_UK_1,
+	NEWS_UK_2,
+} from "../../../sources/audio/podbean/news/uk";
+import {
+	BITES_US,
+	NEWS_US_1,
+	NEWS_US_2,
+	NEWS_US_3,
+	NEWS_US_4,
+} from "../../../sources/audio/podbean/news/us";
+import {
+	BITES_WORLD,
+	NEWS_WORLD_1,
+	NEWS_WORLD_2,
+} from "../../../sources/audio/podbean/news/world";
+import { UK_1, UK_2, UK_3 } from "../../../sources/news/articles/uk";
+import {
+	US_1,
+	US_2,
+	US_3,
+	US_4,
+	US_5,
+} from "../../../sources/news/articles/us";
+import {
+	WORLD_1,
+	WORLD_2,
+	WORLD_3,
+	WORLD_4,
+	WORLD_5,
+	WORLD_6,
+	WORLD_7,
+} from "../../../sources/news/articles/world";
+import { UK_VIDEO, UK_VIDEO_2 } from "../../../sources/news/videos/uk";
+import { US_VIDEO, US_VIDEO_2 } from "../../../sources/news/videos/us";
+import { WORLD_VIDEO, WORLD_VIDEO_2 } from "../../../sources/news/videos/world";
 import { fetchArticles } from "../../articles/fetch-articles";
 import { fetchYoutubeArticles } from "../../articles/fetch-youtube-articles";
 import { fetchCollections } from "../../collections/fetch-collections";
@@ -16,9 +47,9 @@ import { CRON_TIMES } from "../create-cron";
 import { fetchPodcasts } from "../loaders/fetchPodcasts";
 import { CronConfig } from "../types";
 
-const fetchRSS = (src: any) =>
+const fetchRSS = (srcs: any[]) =>
 	fetchCollections({
-		sources: [src], //, US, WORLD
+		sources: srcs, //, US, WORLD
 		feedCallback: getCollection,
 		itemsCallback: fetchArticles,
 	});
@@ -34,6 +65,22 @@ const fetchYoutubeRSS = (srcs: any[]) =>
 		},
 	});
 
+const videos1 = [UK_VIDEO, US_VIDEO, WORLD_VIDEO];
+const videos2 = [UK_VIDEO_2, US_VIDEO_2, WORLD_VIDEO_2];
+const videos = [...videos1, ...videos2];
+
+const podcasts_features = [DIDDY_TRIAL];
+const podcasts_uk = [NEWS_UK_1, NEWS_UK_2];
+const podcasts_us = [NEWS_US_1, NEWS_US_2, NEWS_US_3, NEWS_US_4];
+const podcasts_world = [NEWS_WORLD_1, NEWS_WORLD_2];
+const podcast_bites = [BITES_UK, BITES_US, BITES_WORLD];
+
+const uk_articles = [UK_1, UK_2, UK_3];
+const us_articles_1 = [US_1, US_2];
+const us_articles_2 = [US_3, US_4, US_5];
+const world_articles_1 = [WORLD_1, WORLD_2];
+const world_articles_2 = [WORLD_3, WORLD_4, WORLD_5];
+const world_articles_3 = [WORLD_5, WORLD_6, WORLD_7];
 // you could functionalise
 // pass in cron, fetch, and complete
 export const rssCronConfig: CronConfig = {
@@ -42,22 +89,52 @@ export const rssCronConfig: CronConfig = {
 	cron: [
 		{
 			time: CRON_TIMES.fifthteen,
-			fetchFn: fetchRSS(UK_3),
+			fetchFn: fetchRSS(uk_articles),
 			onComplete: () => {},
 		},
 		{
 			time: CRON_TIMES.fifthteen_1,
-			fetchFn: fetchRSS(US_5),
+			fetchFn: fetchRSS(us_articles_1),
 			onComplete: () => {},
 		},
 		{
 			time: CRON_TIMES.fifthteen_2,
-			fetchFn: fetchRSS(WORLD_7),
+			fetchFn: fetchRSS(us_articles_2),
 			onComplete: () => {},
 		},
 		{
 			time: CRON_TIMES.fifthteen_3,
-			fetchFn: fetchYoutubeRSS([UK_VIDEO_2, US_VIDEO_2, WORLD_VIDEO_2]),
+			fetchFn: fetchYoutubeRSS(world_articles_1),
+			onComplete: () => {},
+		},
+		{
+			time: CRON_TIMES.fifthteen_4,
+			fetchFn: fetchYoutubeRSS(world_articles_2),
+			onComplete: () => {},
+		},
+		{
+			time: CRON_TIMES.fifthteen_5,
+			fetchFn: fetchYoutubeRSS(world_articles_3),
+			onComplete: () => {},
+		},
+		{
+			time: CRON_TIMES.fifthteen_6,
+			fetchFn: fetchYoutubeRSS(videos),
+			onComplete: () => {},
+		},
+		{
+			time: CRON_TIMES.fifthteen_7,
+			fetchFn: fetchYoutubeRSS([...podcasts_features, podcasts_uk]),
+			onComplete: () => {},
+		},
+		{
+			time: CRON_TIMES.fifthteen_8,
+			fetchFn: fetchYoutubeRSS(podcasts_us),
+			onComplete: () => {},
+		},
+		{
+			time: CRON_TIMES.fifthteen_9,
+			fetchFn: fetchYoutubeRSS([...podcasts_world, ...podcast_bites]),
 			onComplete: () => {},
 		},
 	],
