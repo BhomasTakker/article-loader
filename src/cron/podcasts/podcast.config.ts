e025f51a567855369a572e35a3dd1555cf113040
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-05-11 09:47:20 +0100

    update podcasts cron config

--- src/cron/podcasts/podcast.config.ts
+++ src/cron/podcasts/podcast.config.ts
@@ -1,3 +1,4 @@
+import { DIDDY_TRIAL } from "../../../sources/audio/podbean/news/features";
 import {
 	BITES_UK,
 	NEWS_UK_1,
@@ -15,6 +16,7 @@ import {
 	NEWS_WORLD_1,
 	NEWS_WORLD_2,
 } from "../../../sources/audio/podbean/news/world";
+import { CRON_TIMES } from "../create-cron";
 import { fetchPodcasts } from "../loaders/fetchPodcasts";
 import { CronConfig } from "../types";
 
@@ -23,58 +25,63 @@ export const podcastRssCronConfig: CronConfig = {
 	anyCommandsRequired: {},
 	cron: [
 		{
-			time: "0,15,30,45 * * * *",
+			time: CRON_TIMES.fifthteen,
 			fetchFn: fetchPodcasts([BITES_UK]),
 			onComplete: () => {},
 		},
 		{
-			time: "1,16,31,46 * * * *",
+			time: CRON_TIMES.fifthteen_1,
 			fetchFn: fetchPodcasts([BITES_US]),
 			onComplete: () => {},
 		},
 		{
-			time: "2,17,32,47 * * * *",
+			time: CRON_TIMES.fifthteen_2,
 			fetchFn: fetchPodcasts([BITES_WORLD]),
 			onComplete: () => {},
 		},
 		{
-			time: "3 */6 * * *",
+			time: CRON_TIMES.hours_6,
 			fetchFn: fetchPodcasts([NEWS_UK_1]),
 			onComplete: () => {},
 		},
 		{
-			time: "4 */6 * * *",
+			time: CRON_TIMES.hours_6_1,
 			fetchFn: fetchPodcasts([NEWS_UK_2]),
 			onComplete: () => {},
 		},
 		{
-			time: "5 */6 * * *",
+			time: CRON_TIMES.hours_6_2,
 			fetchFn: fetchPodcasts([NEWS_US_1]),
 			onComplete: () => {},
 		},
 		{
-			time: "6 */6 * * *",
+			time: CRON_TIMES.hours_6_3,
 			fetchFn: fetchPodcasts([NEWS_US_2]),
 			onComplete: () => {},
 		},
 		{
-			time: "7 */6 * * *",
+			time: CRON_TIMES.hours_6_4,
 			fetchFn: fetchPodcasts([NEWS_US_3]),
 			onComplete: () => {},
 		},
 		{
-			time: "8 */6 * * *",
+			time: CRON_TIMES.hours_6_5,
 			fetchFn: fetchPodcasts([NEWS_US_4]),
 			onComplete: () => {},
 		},
 		{
-			time: "9 */6 * * *",
+			time: CRON_TIMES.hours_6_7,
 			fetchFn: fetchPodcasts([NEWS_WORLD_1]),
 			onComplete: () => {},
 		},
 		{
-			time: "10 */6 * * *",
+			time: CRON_TIMES.hours_6_8,
 			fetchFn: fetchPodcasts([NEWS_WORLD_2]),
+			onComplete: () => {},
+		},
+		{
+			time: CRON_TIMES.hours_6_9,
+			fetchFn: fetchPodcasts([DIDDY_TRIAL]),
 			onComplete: () => {},
 		},
 	],
