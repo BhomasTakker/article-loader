4599293d84b32a248ca5ff13fde46cbb053c2545
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-07-26 19:10:00 +0100

    Basic add 2 states

---
+++ sources/news/articles/united-states/colorado.ts
@@ -0,0 +1,22 @@
+import { NewsSources } from "../../sources";
+
+export const COLORADO_ARTICLES = {
+	categories: ["news"],
+	collectionType: "news",
+	region: ["North America", "US", "Colorado"],
+	coverage: ["regional", "local"],
+	sources: [
+		{
+			name: NewsSources.DENVER_POST,
+			src: "https://www.denverpost.com/feed/",
+			coverage: ["local"],
+			region: ["Colorado", "Denver"],
+		},
+		{
+			name: NewsSources.DENVER_POST,
+			src: "https://www.denverpost.com/news/colorado/feed/",
+			coverage: ["regional"],
+			region: ["Colorado"],
+		},
+	],
+};
