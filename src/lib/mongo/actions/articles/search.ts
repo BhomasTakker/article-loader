85999633b9510dd50eae9a76a46977c990de411c
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-07-27 18:27:43 +0100

    update search query

--- src/lib/mongo/actions/articles/search.ts
+++ src/lib/mongo/actions/articles/search.ts
@@ -5,9 +5,6 @@ import { buildArticleSearchQuery } from "./query";
 const CACHE_TIME = 60 * 60;
 
 export type GetLatestArticlesProps = {
-	// must, mustNot, filter, should
-	query?: string;
-
 	mustContain?: string[];
 	mustNotContain?: string[];
 	shouldContain?: string[];
@@ -19,14 +16,22 @@ export type GetLatestArticlesProps = {
 	textScore?: string;
 	contentType?: string;
 
+	// details
+	categories?: string;
+
+	// media
+	mediaType?: string;
+
 	// can't at the moment
 	provider?: string;
 	origin?: string;
 
 	variant?: string;
 
+	// time based
 	before?: Date;
 	after?: Date;
+	within?: string;
 
 	// cannot query provider at the moment
 	// search index etc required
@@ -37,7 +42,24 @@ export type GetLatestArticlesProps = {
 
 	durationHigher?: string;
 	durationLower?: string;
+
 	region?: string;
+	// coverage used for scoping articles.
+	// i.e. local news is IN US
+	// but I want US && national news - maybe add sub-local
+	coverage?: "international" | "national" | "regional" | "local";
+	// should be temp - we want region to be a  multi select
+	// with region - this and this
+	// this or this
+	// not this and not this
+	// i.e. filter for Birmingham filter NOT Alabama i.e.
+	// with live search to select continent, country, state, city, etc
+	// all as a 'region'
+	continent?: string;
+	country?: string;
+	state?: string;
+	city?: string;
+
 	language?: string;
 
 	sort?: string;
