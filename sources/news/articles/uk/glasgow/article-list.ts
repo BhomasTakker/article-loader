97db22518fc1f87a9c9480e9ce3831b4cbf3c846
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-07-31 21:17:21 +0100

    add galsgee

---
+++ sources/news/articles/uk/glasgow/article-list.ts
@@ -0,0 +1,26 @@
+import { NewsSources } from "../../../sources";
+import { glasgowUKSources } from "./sources";
+
+export const GLASGOW_SOURCES = {
+	categories: ["news"],
+	region: ["UK", "Scotland", "West", "Glasgow"],
+	coverage: ["local"],
+	sources: [
+		{
+			name: glasgowUKSources.GLASGOW_LIVE,
+			src: "https://www.glasgowlive.co.uk/news/?service=rss",
+		},
+		{
+			name: glasgowUKSources.GLASGOW_TIMES,
+			src: "https://www.glasgowtimes.co.uk/news/rss/",
+		},
+		{
+			name: NewsSources.BBC,
+			src: "https://feeds.bbci.co.uk/news/scotland/glasgow_and_west/rss.xml",
+		},
+		{
+			name: NewsSources.STV,
+			src: "https://news.stv.tv/section/west-central/feed",
+		},
+	],
+};
