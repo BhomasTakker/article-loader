d19d973f2832c5a04cadc639efe6b2f2ff726579
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-07-31 20:18:51 +0100

    update uk sources list

--- sources/news/articles/uk/sources.ts
+++ sources/news/articles/uk/sources.ts
@@ -1,7 +1,15 @@
+import { birminghamUKSources, birminghamUKSourcesMap } from "./birmingham";
+import { liverpoolUKSources, liverpoolUKSourcesMap } from "./liverpool";
 import { manchesterUKSources, manchesterUKSourcesMap } from "./manchester";
 
 export const ukSources = {
+	...birminghamUKSources,
+	...liverpoolUKSources,
 	...manchesterUKSources,
 };
 
-export const ukSourcesMap = new Map([...manchesterUKSourcesMap]);
+export const ukSourcesMap = new Map([
+	...manchesterUKSourcesMap,
+	...birminghamUKSourcesMap,
+	...liverpoolUKSourcesMap,
+]);
