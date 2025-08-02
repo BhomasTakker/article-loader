20af95bfa3a26d4f021091112e25afb867db734d
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-08-02 13:38:02 +0100

    add ni sources

---
+++ sources/news/articles/uk/northern-ireland/article-list.ts
@@ -0,0 +1,58 @@
+import { NewsSources } from "../../../sources";
+import { northernIrelandSources } from "./sources";
+
+export const NORTHERN_IRELAND_ARTICLE_SOURCES = {
+	categories: ["news"],
+	region: ["UK", "NI"],
+	coverage: ["regional"],
+	language: "en",
+	sources: [
+		{
+			name: northernIrelandSources.NORTHERN_IRELAND_WORLD,
+			src: "https://www.northernirelandworld.com/rss",
+			region: ["UK", "NI"],
+			coverage: ["regional"],
+		},
+		{
+			name: northernIrelandSources.THE_IRISH_NEWS,
+			src: "https://www.irishnews.com/arc/outboundfeeds/rss/",
+			region: ["UK", "NI"],
+			coverage: ["regional"],
+		},
+		{
+			name: northernIrelandSources.BELFAST_LIVE,
+			src: "https://www.belfastlive.co.uk/news/?service=rss",
+			region: ["UK", "NI"],
+			coverage: ["national", "regional"],
+		},
+		{
+			name: northernIrelandSources.BELFAST_LIVE,
+			src: "https://www.belfastlive.co.uk/news/belfast-news/?service=rss",
+			region: ["UK", "NI", "Belfast"],
+			coverage: ["regional", "local"],
+		},
+		{
+			name: NewsSources.BBC,
+			src: "https://feeds.bbci.co.uk/news/northern_ireland/rss.xml",
+		},
+	],
+};
+
+export const northernIrelandVideoSources = {
+	categories: ["news"],
+	region: ["UK", "NI"],
+	coverage: ["regional"],
+	language: "en",
+	sources: [
+		{
+			name: northernIrelandSources.BELFAST_LIVE,
+			src: "https://www.youtube.com/feeds/videos.xml?channel_id=UCPi8u-XgLfuD52rT2m85sUw",
+			coverage: ["national", "regional", "local"],
+		},
+		{
+			name: northernIrelandSources.UTV,
+			src: "https://www.youtube.com/feeds/videos.xml?channel_id=UCBM06gTNsYKeyuAXIhIUW9Q",
+			coverage: ["national", "regional"],
+		},
+	],
+};
---
+++ sources/news/articles/uk/northern-ireland/sources.ts
@@ -0,0 +1,53 @@
+const northernIrelandWorld = {
+	origin: "UK",
+	name: "Northern Ireland World",
+	description: "Northern Ireland World is a regional news website.",
+	url: "https://northernirelandworld.com/",
+	logo: "https://www.northernirelandworld.com/img/placeholder.png",
+	rating: 80,
+	leaning: 0,
+};
+
+const theIrish = {
+	origin: "UK",
+	name: "The Irish News",
+	description: "The Irish News is a regional news website.",
+	url: "https://www.irishnews.com/",
+	logo: "https://www.irishnews.com/resources/images/irishnews-logo.png",
+	rating: 80,
+	leaning: 0,
+};
+
+const belfastLive = {
+	origin: "UK",
+	name: "Belfast Live",
+	description: "Belfast Live is a regional news website.",
+	url: "https://www.belfastlive.co.uk/",
+	logo: "https://www.belfastlive.co.uk/resources/images/belfastlive-logo.png",
+	rating: 80,
+	leaning: 0,
+};
+
+const utv = {
+	origin: "UK",
+	name: "UTV",
+	description: "UTV is a regional news website.",
+	url: "https://www.u.tv/",
+	logo: "https://www.u.tv/resources/images/utv-logo.png",
+	rating: 75,
+	leaning: 0,
+};
+
+export const northernIrelandSources = {
+	BELFAST_LIVE: "Belfast Live",
+	THE_IRISH_NEWS: "The Irish News",
+	NORTHERN_IRELAND_WORLD: "Northern Ireland World",
+	UTV: "UTV",
+};
+
+export const northernIrelandSourcesMap = new Map([
+	[northernIrelandSources.NORTHERN_IRELAND_WORLD, northernIrelandWorld],
+	[northernIrelandSources.THE_IRISH_NEWS, theIrish],
+	[northernIrelandSources.BELFAST_LIVE, belfastLive],
+	[northernIrelandSources.UTV, utv],
+]);
