c1af0575cf37ad30665bf8114d0e6ff8ead1a144
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-08-02 13:00:59 +0100

    create wales sources

--- sources/news/articles/uk/wales/sources.ts
+++ sources/news/articles/uk/wales/sources.ts
@@ -1,3 +1,142 @@
-export const walesSources = {};
+export const walesOnline = {
+	origin: "UK",
+	name: "Wales Online",
+	description: "Wales Online is a regional news website.",
+	url: "https://www.walesonline.co.uk/",
+	logo: "https://s2-prod.walesonline.co.uk/@trinitymirrordigital/chameleon-branding/publications/walesonline/img/logo-walesonline-social-sharing.png",
+	rating: 85,
+	leaning: -0.2,
+};
+export const heraldWales = {
+	origin: "UK",
+	name: "Herald Wales",
+	description: "Herald Wales is a regional news website.",
+	url: "https://herald.wales/",
+	logo: "https://herald.wales/wp-content/uploads/2020/11/cropped-herald-wales-logo-1.png",
+	rating: 80,
+	leaning: -0.1,
+};
+export const powysCountyTimes = {
+	origin: "UK",
+	name: "Powys County Times",
+	description: "Powys County Times is a local newspaper in Powys.",
+	url: "https://www.countytimes.co.uk/",
+	logo: "https://www.countytimes.co.uk/resources/images/county-times-logo.png",
+	rating: 75,
+	leaning: -0.1,
+};
+export const wales247 = {
+	origin: "UK",
+	name: "Wales 247",
+	description: "Wales 247 is a regional news website.",
+	url: "https://www.wales247.co.uk/",
+	logo: "https://www.wales247.co.uk/wp-content/uploads/2020/11/cropped-wales-247-logo-1.png",
+	rating: 80,
+	leaning: -0.1,
+};
+export const nationCymru = {
+	origin: "UK",
+	name: "Nation Cymru",
+	description: "Nation Cymru is a Welsh news website.",
+	url: "https://nation.cymru/",
+	logo: "https://nation.cymru/wp-content/uploads/2020/11/cropped-nation-cymru-logo-1.png",
+	rating: 80,
+	leaning: -0.1,
+};
+export const northWalesLive = {
+	origin: "UK",
+	name: "North Wales Live",
+	description: "North Wales Live is a regional news website.",
+	url: "https://www.dailypost.co.uk/",
+	logo: "https://www.dailypost.co.uk/resources/images/daily-post-logo.png",
+	rating: 80,
+	leaning: -0.1,
+};
+export const southWalesGuardian = {
+	origin: "UK",
+	name: "South Wales Guardian",
+	description: "South Wales Guardian is a local newspaper in South Wales.",
+	url: "https://www.southwalesguardian.co.uk/",
+	logo: "https://www.southwalesguardian.co.uk/resources/images/south-wales-guardian-logo.png",
+	rating: 75,
+	leaning: -0.1,
+};
+export const southWalesArgus = {
+	origin: "UK",
+	name: "South Wales Argus",
+	description: "South Wales Argus is a local newspaper in South Wales.",
+	url: "https://www.southwalesargus.co.uk/",
+	logo: "https://www.southwalesargus.co.uk/resources/images/south-wales-argus-logo.png",
+	rating: 75,
+	leaning: -0.1,
+};
 
-export const walesSourceMap = new Map([]);
+export const cardiffTv = {
+	origin: "UK",
+	name: "Cardiff TV",
+	description: "Cardiff TV is a local television channel in Cardiff.",
+	url: "https://www.cardiff-tv.co.uk/",
+	logo: "https://www.cardiff-tv.co.uk/logo.png",
+	rating: 70,
+	leaning: -0.1,
+};
+
+export const theCardiffPost = {
+	origin: "UK",
+	name: "Cardiff Post",
+	description: "Cardiff Post is a local news website in Cardiff.",
+	url: "https://www.cardiffpost.co.uk/",
+	logo: "https://www.cardiffpost.co.uk/logo.png",
+	rating: 70,
+	leaning: -0.1,
+};
+
+export const thePenarthTimes = {
+	origin: "UK",
+	name: "Penarth Times",
+	description: "Penarth Times is a local newspaper in Penarth.",
+	url: "https://www.penarthtimes.co.uk/",
+	logo: "https://www.penarthtimes.co.uk/resources/images/penarth-times-logo.png",
+	rating: 70,
+	leaning: -0.1,
+};
+
+export const swanseaBayNews = {
+	origin: "UK",
+	name: "Swansea Bay News",
+	description: "Swansea Bay News is a local news website in Swansea.",
+	url: "https://www.swanseabaynews.com/",
+	logo: "https://www.swanseabaynews.com/logo.png",
+	rating: 70,
+	leaning: -0.1,
+};
+
+export const walesSources = {
+	WALES_ONLINE: "Wales Online",
+	HERALD_WALES: "Herald Wales",
+	POWYS_COUNTY_TIMES: "Powys County Times",
+	WALES_247: "Wales 247",
+	NATION_CYMRU: "Nation Cymru",
+	NORTH_WALES_LIVE: "North Wales Live",
+	SOUTH_WALES_GUARDIAN: "South Wales Guardian",
+	SOUTH_WALES_ARGUS: "South Wales Argus",
+	CARDIFF_TV: "Cardiff TV",
+	THE_CARDIFF_POST: "Cardiff Post",
+	THE_PENARTH_TIMES: "Penarth Times",
+	SWANSEA_BAY_NEWS: "Swansea Bay News",
+};
+
+export const walesSourceMap = new Map([
+	[walesSources.WALES_ONLINE, walesOnline],
+	[walesSources.HERALD_WALES, heraldWales],
+	[walesSources.POWYS_COUNTY_TIMES, powysCountyTimes],
+	[walesSources.WALES_247, wales247],
+	[walesSources.NATION_CYMRU, nationCymru],
+	[walesSources.NORTH_WALES_LIVE, northWalesLive],
+	[walesSources.SOUTH_WALES_GUARDIAN, southWalesGuardian],
+	[walesSources.SOUTH_WALES_ARGUS, southWalesArgus],
+	[walesSources.CARDIFF_TV, cardiffTv],
+	[walesSources.THE_CARDIFF_POST, theCardiffPost],
+	[walesSources.THE_PENARTH_TIMES, thePenarthTimes],
+	[walesSources.SWANSEA_BAY_NEWS, swanseaBayNews],
+]);
