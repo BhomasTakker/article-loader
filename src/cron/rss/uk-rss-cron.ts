cedada0f38f23c22247b92b440dfeeccc8025c7b
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-08-02 19:38:31 +0100

    don't leave in 30 seconds cron

--- src/cron/rss/uk-rss-cron.ts
+++ src/cron/rss/uk-rss-cron.ts
@@ -14,40 +14,40 @@ export const ukRssCronConfig: CronConfig = {
 	id: "RSS Cron Queries",
 	anyCommandsRequired: {},
 	cron: [
-		{
-			time: staggerSeconds(30, 0),
-			fetchFn: fetchRSS(ukNationalArticles1),
-			onComplete: () => {},
-		},
 		// {
-		// 	time: staggerMinutes(15, 0),
+		// 	time: staggerSeconds(30, 0),
 		// 	fetchFn: fetchRSS(ukNationalArticles1),
 		// 	onComplete: () => {},
 		// },
-		// {
-		// 	time: staggerMinutes(15, 1),
-		// 	fetchFn: fetchRSS(ukNationalArticles2),
-		// 	onComplete: () => {},
-		// },
-		// {
-		// 	time: staggerMinutes(15, 2),
-		// 	fetchFn: fetchYoutubeRSS(ukNationalVideos),
-		// 	onComplete: () => {},
-		// },
-		// {
-		// 	time: everyNthHour(24, 5),
-		// 	fetchFn: fetchYoutubeRSS(ukLiveVideos),
-		// 	onComplete: () => {},
-		// },
-		// {
-		// 	time: everyNthHour(1, 0),
-		// 	fetchFn: fetchRSS(ukRegionalArticles),
-		// 	onComplete: () => {},
-		// },
-		// {
-		// 	time: everyNthHour(1, 1),
-		// 	fetchFn: fetchYoutubeRSS(ukRegionalVideos),
-		// 	onComplete: () => {},
-		// },
+		{
+			time: staggerMinutes(15, 0),
+			fetchFn: fetchRSS(ukNationalArticles1),
+			onComplete: () => {},
+		},
+		{
+			time: staggerMinutes(15, 1),
+			fetchFn: fetchRSS(ukNationalArticles2),
+			onComplete: () => {},
+		},
+		{
+			time: staggerMinutes(15, 2),
+			fetchFn: fetchYoutubeRSS(ukNationalVideos),
+			onComplete: () => {},
+		},
+		{
+			time: everyNthHour(24, 5),
+			fetchFn: fetchYoutubeRSS(ukLiveVideos),
+			onComplete: () => {},
+		},
+		{
+			time: everyNthHour(1, 0),
+			fetchFn: fetchRSS(ukRegionalArticles),
+			onComplete: () => {},
+		},
+		{
+			time: everyNthHour(1, 1),
+			fetchFn: fetchYoutubeRSS(ukRegionalVideos),
+			onComplete: () => {},
+		},
 	],
 };
