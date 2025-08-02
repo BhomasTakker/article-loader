d879ffafc36252d93e759216d754aff3e731ea53
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-08-02 20:52:33 +0100

    add to england sources

--- sources/news/articles/uk/england/sources.ts
+++ sources/news/articles/uk/england/sources.ts
@@ -1,14 +1,18 @@
 import { birminghamUKSources, birminghamUKSourcesMap } from "./birmingham";
 import { liverpoolUKSources, liverpoolUKSourcesMap } from "./liverpool";
 import { manchesterUKSources, manchesterUKSourcesMap } from "./manchester";
+import { yorkshireSources, yorkshireSourcesMap } from "./yorkshire";
 
+// probably west mids, east mids, east anglia, south east, south west, north east, north west
 export const englandSources = {
 	...birminghamUKSources,
 	...manchesterUKSources,
 	...liverpoolUKSources,
+	...yorkshireSources,
 };
 export const englandSourcesMap = new Map([
 	...birminghamUKSourcesMap,
 	...manchesterUKSourcesMap,
 	...liverpoolUKSourcesMap,
+	...yorkshireSourcesMap,
 ]);
