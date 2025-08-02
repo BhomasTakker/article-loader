9eb56eba679e373bdc16ed690974304d06ceae1e
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-08-02 15:38:19 +0100

    create uk cron

---
+++ src/cron/rss/uk-rss-cron.ts
@@ -0,0 +1,48 @@
+import {
+	ukLiveVideos,
+	ukNationalArticles1,
+	ukNationalArticles2,
+	ukNationalVideos,
+	ukRegionalArticles,
+	ukRegionalVideos,
+} from "../../../sources/news/articles/uk/national";
+import { everyNthHour, staggerMinutes } from "../create-cron";
+import { CronConfig } from "../types";
+import { fetchRSS, fetchYoutubeRSS } from "./utils";
+
+export const ukRssCronConfig: CronConfig = {
+	id: "RSS Cron Queries",
+	anyCommandsRequired: {},
+	cron: [
+		{
+			time: staggerMinutes(15, 0),
+			fetchFn: fetchRSS(ukNationalArticles1),
+			onComplete: () => {},
+		},
+		{
+			time: staggerMinutes(15, 1),
+			fetchFn: fetchRSS(ukNationalArticles2),
+			onComplete: () => {},
+		},
+		{
+			time: staggerMinutes(15, 2),
+			fetchFn: fetchYoutubeRSS(ukNationalVideos),
+			onComplete: () => {},
+		},
+		{
+			time: everyNthHour(24, 5),
+			fetchFn: fetchYoutubeRSS(ukLiveVideos),
+			onComplete: () => {},
+		},
+		{
+			time: everyNthHour(1, 0),
+			fetchFn: fetchRSS(ukRegionalArticles),
+			onComplete: () => {},
+		},
+		{
+			time: everyNthHour(1, 1),
+			fetchFn: fetchYoutubeRSS(ukRegionalVideos),
+			onComplete: () => {},
+		},
+	],
+};
