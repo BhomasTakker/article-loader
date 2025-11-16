390fee9c4caa2bd9730ca9d8b04dd537afb62914
Thomas Bakker <thomasbakker@hotmail.co.uk>
2025-11-16 18:16:26 +0000

    update index to use new rss loading

--- index.ts
+++ index.ts
@@ -4,8 +4,14 @@ import { logMemoryUsage } from "./src/lib/mem";
 import { initApiRoutes } from "./routes/api-routes";
 import { initCronJobs } from "./src/cron/init-cron";
 import { podcastRssCronConfig } from "./src/cron/podcasts/podcast.config";
-import { rssCronConfig } from "./src/cron/rss/rss-cron";
-import { ukRssCronConfig } from "./src/cron/rss/uk-rss-cron";
+import {
+	createRssCronConfig,
+	initializeRSSSources,
+} from "./src/cron/rss/rss-cron";
+import {
+	createUkRssCronConfig,
+	initializeUKSources,
+} from "./src/cron/rss/uk-rss-cron";
 import { initialiseExpress, startServer } from "./src/services/express";
 import { getEnv } from "./src/services/env";
 import { pageQueriesCronConfig } from "./src/cron/api/search/config";
@@ -13,7 +19,7 @@ import { RADIO_CRON_CONFIG } from "./src/cron/radio/config";
 
 require("dotenv").config();
 
-export const initialiseServer = () => {
+export const initialiseServer = async () => {
 	const app = initialiseExpress();
 	startServer(app);
 
@@ -25,8 +31,15 @@ export const initialiseServer = () => {
 
 	isApiCron && initCronJobs(pageQueriesCronConfig);
 
-	isRSSCron && initCronJobs(rssCronConfig);
-	isRSSCron && initCronJobs(ukRssCronConfig);
+	if (isRSSCron) {
+		await initializeRSSSources();
+		initCronJobs(createRssCronConfig());
+	}
+
+	if (isRSSCron) {
+		await initializeUKSources();
+		initCronJobs(createUkRssCronConfig());
+	}
 
 	isApiCron && initCronJobs(RADIO_CRON_CONFIG);
 };
