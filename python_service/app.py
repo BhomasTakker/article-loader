9e1c50e2079432a18539f7ba1d88c9edc91ca86e
Thomas Bakker <thomasbakker@hotmail.co.uk>
2026-06-27 13:47:38 +0100

    fix stupids stupid mistake

--- python_service/app.py
+++ python_service/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -6,3 +7,7 @@ app = FastAPI()
 @app.get("/ping")
 def ping():
     return {"status": "ok", "service": "python-service"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)
