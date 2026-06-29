2bbaf2855cdf82fa65486ffd4935bd3087e61d30
Thomas Bakker <thomasbakker@hotmail.co.uk>
2026-06-29 01:04:14 +0100

    py 3.13 -> py 3.9

--- python_service/models/article.py
+++ python_service/models/article.py
@@ -1,3 +1,4 @@
+from typing import Optional, List, Dict, Any
 from pydantic import BaseModel, Field
 from datetime import datetime
 from bson import ObjectId
@@ -5,18 +6,18 @@ from .objectid import PyObjectId
 
 class Article(BaseModel):
     id: PyObjectId = Field(alias="_id")
-    title: str | None = None
-    src: str | None = None
-    details: dict | None = None
-    createdAt: datetime | None = None
-    updatedAt: datetime | None = None
-    description: str | None = None
-    avatar: dict | None = None
-    feed: PyObjectId | None = None
-    provider: PyObjectId | None = None
-    guid: str | None = None
-    variant: str | None = None
-    disabled: bool | None = None
+    title: Optional[str] = None
+    src: Optional[str] = None
+    details: Optional[Dict[str, Any]] = None
+    createdAt: Optional[datetime] = None
+    updatedAt: Optional[datetime] = None
+    description: Optional[str] = None
+    avatar: Optional[Dict[str, Any]] = None
+    feed: Optional[PyObjectId] = None
+    provider: Optional[PyObjectId] = None
+    guid: Optional[str] = None
+    variant: Optional[str] = None
+    disabled: Optional[bool] = None
 
     model_config = {
         "populate_by_name": True,
